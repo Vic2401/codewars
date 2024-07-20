e64bc6a592e5e2e1a99aaf05ef26962c11f703b5
Viktor Burman <vburman2401@gmail.com>
2024-07-20 10:34:15 +0200

    feat: solve task

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const numbers = ["1233"];
+let result = [];
 
-function multiplyOfNumbers(arr) {
-  return arr.reduce(function (done, curr) {
-    return done * multiplyOfNumbers(curr);
-  }, 1);
+splitString('hell0 askdmlsa!');
+
+let string;
+
+function splitString(string) {
+  let second = 2;
+
+  for (first = 0; first < string.length; first = first + 2) {
+    if (string.length % 2 !== 0) {
+      string = string + '_';
+    }
+    result.push(string.slice(first, second));
+    second = second + 2;
+  }
+  console.log(result);
+  return result;
 }
-console.log(multiplyOfNumbers(numbers));
-/*
-function solution(s) {
-  return (s + "_").match(/.{2}/g) || [];
-}*/
+
+splitString('hell0');
